fb3b70d684d5369471d5cd49a8fa8d3d44fa4a4a
Cali Dolfi <cdolfi@redhat.com>
2022-04-04 17:57:20 -0400

    search bar and dashbootstrap additions

--- pages/start.py
+++ pages/start.py
@@ -1,6 +1,32 @@
 from dash import html
 from app import app
+import dash
+from dash import dcc
+from dash.dependencies import Output, Input
+import plotly
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import pandas as pd
+import sqlalchemy as salc
+import psycopg2
+import json
+import numpy as np
 
-layout = html.Div(children=[
-    html.H1(children="Start Page!")
-])
+
+
+layout = dbc.Container([
+
+    dbc.Row([
+
+        dbc.Col([
+            html.P("Start page text holder",
+                        className='text-center font-weight-bold mb-4')
+            ],width=12)
+    ]),
+
+    
+
+], fluid=False)
+
+
+
