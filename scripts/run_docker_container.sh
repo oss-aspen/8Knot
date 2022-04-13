fcf806589b4fda8ea6050e0ffff0b38afbc6576e
Sean P. Goggins <s@goggins.com>
2022-04-13 14:26:45 -0500

    Bulk up Docker Defaults.

--- scripts/run_docker_container.sh
+++ scripts/run_docker_container.sh
@@ -1,2 +1,2 @@
 # please run this while in the same folder as the dockerfile 
-sudo docker run --rm -it --name explorer -p 8050:8050 explorer
+sudo docker run --rm -it --name explorer -p 8050:8050 --cpus=2 --memory=8g explorer
