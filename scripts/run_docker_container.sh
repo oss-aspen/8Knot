ae3c7773a75e2e6030f6cae5418a9b1d4e50873d
Sean Goggins <outdoors@acm.org>
2022-09-26 12:04:01 -0500

    Dockeer Trick
    Signed-off-by: Sean Goggins <outdoors@acm.org>

--- scripts/run_docker_container.sh
+++ scripts/run_docker_container.sh
@@ -1,2 +1,2 @@
 # please run this while in the same folder as the dockerfile
-sudo docker run --rm -it -v $PWD:/explorer --name explorer -p 8050:8050 explorer
+sudo docker run --env-file $PWD/env.list --rm -it -v $PWD:/explorer --name explorer -p 8050:8050 explorer
