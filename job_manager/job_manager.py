a14bf7260b4ca2c865b7ae16c7fbc39f61a2aa2b
James Kunstle <jkunstle@bu.edu>
2022-10-06 14:26:40 -0400

    Adds docstrings to job_manager class
    Signed-off-by: James Kunstle <jkunstle@bu.edu>

--- job_manager/job_manager.py
+++ job_manager/job_manager.py
@@ -10,6 +10,44 @@ import logging
 
 
 class JobManager:
+    """
+    A class that wraps the RQ worker interface (enqueuement and return retrieval).
+    Does not abstract the choices-to-be-made based on a job's status and availability.
+
+    Attributes
+    ----------
+        _redis : (private) Redis object
+            Manager for Redis in-memory cache that hosts queue for RQ workers
+            and RQ worker results.
+
+        _rq : (private) Queue object
+            Queue onto which jobs are pushed, FIFO, for RQ workers to pick up.
+
+        
+    Methods
+    -------
+        _get_job_hash(func, arglist) (private) :
+            Creates a unique hash for each job based on the job's calling
+            function and the list of repos that the function is being run with.
+
+        add_job(func, dbmc, repolist) :
+            Enqueues job onto Queue object hosted on Redis for RQ worker to pick
+            up later, keyed by job_hash.
+
+        get_job(func, arglist) :
+            Gets most updated job from Queue object, if its job_hash ID exists.
+
+        get_results(func, arglist) :
+            Returns the status of a job and the results from that job, if they exist.
+
+        job_refreshed(job) : 
+            Returns job-object refreshed with most updated contents from Queue, if
+            it exists.
+
+        get_job_status(func, arglist) :
+            Returns a job's status, if it exists.
+    
+    """
     def __init__(self):
         # Redis cache for job queue and results cache
         self._redis = Redis(
@@ -22,32 +60,80 @@ class JobManager:
         self._rq = Queue(connection=self._redis)
 
     def _get_job_hash(self, func, arglist):
+        """
+        (private)
+        Creates an MD5-hash based on the string-bytes
+        of the passed function and the list of repos that
+        the function is going to be run on.
+
+        Hash is used as a key by which the status of and results from the
+        worker are accessed from the Queue.
+        
+        Args:
+        -----
+            func (function): Function that worker picks up to run as job.
+            arglist ([str]): Arguments to function, list of repos.
+
+        Returns:
+        --------
+            _Hash: Unique key in Queue object to access job status and results. 
+        """
 
         # use md5 instead of sha256 or better because
         # we're only ensuring limited collision avoidance, not
         # practicing good securiy protocol.
         hashfunc = hashlib.md5()
+
         # use the called function's name
         hashfunc.update(bytes(func.__name__, "utf-8"))
+
         # and the repo list we're passing to it
         hashfunc.update(bytes(str(frozenset(arglist)), "utf-8"))
+
         # grab the hex hash that's been generated.
         h = hashfunc.hexdigest()
 
         return h
 
-    # dbmc is "database manager config"
     def add_job(self, func, dbmc, repolist):
+        """
+        Adds job to Queue object hosted in Redis.
+
+        Args:
+        -----
+            func (function): Function that worker picks up to run as job.
+            dbmc (AugurInterface class): DatabaseManagerClass, handles access to database of community data.
+            repolist ([str]): Arguments to function, list of repos.
+
+        Returns:
+        --------
+            _Hash: Unique key in Queue object to access job status and results.
+        """
 
         # get a hash of the function used and the args supplied
         job_hash = self._get_job_hash(func, repolist)
 
-        # add a job to our queue, 10 minute timeout (6000ms), id of it's hash.
+        # add a job to our queue, 10 minute timeout (6000ms), id of its hash.
         job = self._rq.enqueue(func, dbmc, repolist, job_id=job_hash, job_timeout=6000)
 
         return job_hash
 
     def get_job(self, func, arglist):
+        """
+        Gets job object from Queue object in Redis instance, if it exists.
+        Keyed by its job_hash.
+
+        Returns None if job isn't in Queue.
+
+        Args:
+        -----
+            func (function): Function that worker picks up to run as job.
+            arglist ([str]): Arguments to function, list of repos.
+
+        Returns:
+        --------
+            Job | None: Reference to Job object or None 
+        """
 
         # get an identifying hash of the function used and the args supplied
         job_hash = self._get_job_hash(func, arglist)
@@ -64,6 +150,22 @@ class JobManager:
             return None
 
     def get_results(self, func, arglist):
+        """
+        Gets status and results from Job in Queue object,
+        if job exists.
+
+        If Job doesn't exist by job_hash, returns None.
+
+        Args:
+        -----
+            func (function): Function that worker picks up to run as job.
+            arglist ([str]): Arguments to function, list of repos.
+
+        Returns:
+        --------
+            state (str) | None: State of the Job object in the Queue or None
+            results ([[Any]]) | None: Results from the worker's finished Job, is a list of lists, or None.
+        """
 
         # check to see if job already exists.
         job = self.get_job(func, arglist)
@@ -95,6 +197,17 @@ class JobManager:
             return (None, None)
 
     def job_refreshed(self, job):
+        """
+        Refreshes Job object metadata if the job exists in the Queue.
+
+        Args:
+        -----
+            job (Job): Job object
+
+        Returns:
+        --------
+            Job | None: Refreshed Job object with updated metadata or None.
+        """
         try:
             # try to grab all of the new object values for a job
             job.refresh()
@@ -106,6 +219,20 @@ class JobManager:
             return None
 
     def get_job_status(self, func, arglist):
+        """
+        Current status of Job in Queue, if it exists.
+        
+        None if Job not in Queue.
+
+        Args:
+        -----
+            func (function): Function that worker picks up to run as job.
+            arglist ([str]): Arguments to function, list of repos.
+
+        Returns:
+        --------
+            state (str) | None: State of the Job object in the Queue or None
+        """
 
         # check to see if job exists.
         job = self.get_job(func, arglist)
