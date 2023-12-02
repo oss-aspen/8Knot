8a64f131935da66d33b3ce76f638bb4fa8abb0f8
JoshGreen0 <jwgktd@umsystem.edu>
2023-12-02 14:47:11 -0600

    Joshua-Version-added
    pr_closure_ratio

--- 8Knot/pages/starterprojecthealth/starterprojecthealth.py
+++ 8Knot/pages/starterprojecthealth/starterprojecthealth.py
@@ -8,6 +8,7 @@ from .visualizations.time_first_response import gc_time_first_response
 from .visualizations.change_request_closure_ratio import gc_change_request_closure_ratio
 from .visualizations.bus_factor import gc_bus_factor
 from .visualizations.release_frequency import gc_release_frequency
+from .visualizations.pr_closure_ratio import gc_pr_closure_ratio
 
 warnings.filterwarnings("ignore")
 
@@ -27,6 +28,13 @@ layout = dbc.Container(
             [
                 dbc.Col(gc_bus_factor, width=6),
                 dbc.Col(gc_release_frequency, width=6),
+            ],
+            align="center",
+            style={"marginBottom": ".5%"},
+        ),
+        dbc.Row(
+            [
+                dbc.Col(gc_pr_closure_ratio, width=6),
             ],
             align="center",
             style={"marginBottom": ".5%"},
