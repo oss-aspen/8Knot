d8e1ad8481c2b5c4c13b98184cb5056d69f9faf4
Caio Fonseca <engcaiofonseca@protonmail.com>
2025-05-29 16:44:43 +0100

    Removing search bar and fixing search logic

--- 8Knot/pages/index/index_layout.py
+++ 8Knot/pages/index/index_layout.py
@@ -260,6 +260,17 @@ search_bar = html.Div(
                             className="searchbar-dropdown",
                         ),
                         dbc.Alert(
+                            children='Please ensure that your spelling is correct. \
+                                If your selection definitely isn\'t present, please request that \
+                                it be loaded using the help button "REPO/ORG Request" \
+                                in the bottom right corner of the screen.',
+                            id="help-alert",
+                            dismissable=True,
+                            fade=True,
+                            is_open=False,
+                            color="info",
+                        ),
+                        dbc.Alert(
                             children="List of repos",
                             id="repo-list-alert",
                             dismissable=True,
