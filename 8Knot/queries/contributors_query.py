09a189c4c1b3882d94bd5327b7c594dd76a30461
Asish Kumar <officialasishkumar@gmail.com>
2025-08-09 22:29:07 +0530

    fix: import issue
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- 8Knot/queries/contributors_query.py
+++ 8Knot/queries/contributors_query.py
@@ -83,3 +83,10 @@ def contributors_query(self, repos):
     """
 
     logging.warning(f"{contributors_query.__name__} COLLECTION - END")
+
+def contributors_funnel_query(repolist):
+    return """
+        SELECT *
+        FROM augur_data.d1_contributor_engagement
+        WHERE repo_id IN ({})
+    """.format(",".join([str(r) for r in repolist]))
