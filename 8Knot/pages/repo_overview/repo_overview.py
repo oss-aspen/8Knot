0f1b09e3db5a2762954a2d8f4da2e74601939ed9
Adrian Edwards <adredwar@redhat.com>
2025-09-10 17:30:21 -0400

    convert strings from single-repo selection bar back to integers in callbacks for use

--- 8Knot/pages/repo_overview/repo_overview.py
+++ 8Knot/pages/repo_overview/repo_overview.py
@@ -77,6 +77,6 @@ def repo_dropdown(repo_ids):
     # array to hold repo_id and git url pairing for dropdown
     data_array = []
     for repo_id in repo_ids:
-        entry = {"value": str(repo_id), "label": augur.repo_id_to_git(repo_id)}
+        entry = {"value": str(repo_id), "label": augur.repo_id_to_git(int(repo_id))}
         data_array.append(entry)
     return data_array, str(repo_ids[0])
