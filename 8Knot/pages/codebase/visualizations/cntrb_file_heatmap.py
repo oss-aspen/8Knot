0417994f8d61fcfe2e0e33fa399a7b16f10790ed
Caio Fonseca <engcaiofonseca@protonmail.com>
2026-01-29 19:59:47 -0300

    refactoring two heatmaps, to reduce duplication, creating parameterized function, and adding more support functions into heatmap utils file

--- 8Knot/pages/codebase/visualizations/cntrb_file_heatmap.py
+++ 8Knot/pages/codebase/visualizations/cntrb_file_heatmap.py
@@ -1,25 +1,17 @@
 from dash import html, dcc, callback
-import dash
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 from dash.dependencies import Input, Output, State
-import plotly.graph_objects as go
-import pandas as pd
 import logging
-from dateutil.relativedelta import *  # type: ignore
-import plotly.express as px
-from pages.utils.graph_utils import get_graph_time_values, color_seq
-from queries.contributors_query import contributors_query as cnq
-from queries.cntrb_per_file_query import cntrb_per_file_query as cpfq
-from queries.repo_files_query import repo_files_query as rfq
-from app import augur
-from pages.utils.job_utils import nodata_graph
-import pages.utils.preprocessing_utils as preproc_u
 import time
-from dash.exceptions import PreventUpdate
-import app
-import cache_manager.cache_facade as cf
-from .heatmap_utils import create_repo_dropdown, create_directory_dropdown
+from pages.utils.job_utils import nodata_graph
+from .heatmap_utils import (
+    create_repo_dropdown,
+    create_directory_dropdown,
+    get_activity_heatmap_data,
+    process_activity_heatmap,
+    create_activity_heatmap_figure,
+)
 
 PAGE = "codebase"
 VIZ_ID = "cntrb-file-heatmap"
@@ -176,343 +168,24 @@ def directory_dropdown(repo_id):
 )
 def cntrb_file_heatmap_graph(searchbar_repos, repo_id, directory, bot_switch):
     start = time.perf_counter()
-    logging.warning(f"{VIZ_ID}- START")
+    logging.warning(f"{VIZ_ID} - START")
 
     # get dataframes of data from cache
-    df_file, df_actions, df_file_cntbs = multi_query_helper(searchbar_repos, [repo_id])
+    df_file, df_actions, df_file_cntbs = get_activity_heatmap_data(searchbar_repos, [repo_id], VIZ_ID)
 
     # test if there is data
     if df_file.empty or df_actions.empty or df_file_cntbs.empty:
         logging.warning(f"{VIZ_ID} - NO DATA AVAILABLE")
         return nodata_graph
 
-    # function for all data pre processing
-    df = process_data(df_file, df_actions, df_file_cntbs, directory, bot_switch)
+    # process data using shared function (contributor IDs)
+    df = process_activity_heatmap(df_file, df_actions, df_file_cntbs, directory, bot_switch, "cntrb_ids")
 
     # if there are no cntrbs in a directory plot no data graph
     if df.empty:
         return nodata_graph
 
-    fig = create_figure(df)
+    fig = create_activity_heatmap_figure(df)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
-
-
-def multi_query_helper(searchbar_repos, repo):
-    """
-    For cntrb_file_heatmap_graph-
-    hack to put all of the cache-retrieval
-    in the same place temporarily
-    """
-
-    # wait for data to asynchronously download and become available.
-    while not_cached := cf.get_uncached(func_name=rfq.__name__, repolist=repo):
-        logging.warning(f"CONTRIBUTOR FILE HEATMAP - WAITING ON DATA TO BECOME AVAILABLE")
-        time.sleep(0.5)
-
-    # wait for data to asynchronously download and become available.
-    while not_cached := cf.get_uncached(func_name=cnq.__name__, repolist=searchbar_repos):
-        logging.warning(f"CONTRIBUTOR FILE HEATMAP - WAITING ON DATA TO BECOME AVAILABLE")
-        time.sleep(0.5)
-
-    # wait for data to asynchronously download and become available.
-    while not_cached := cf.get_uncached(func_name=cpfq.__name__, repolist=repo):
-        logging.warning(f"CONTRIBUTOR FILE HEATMAP - WAITING ON DATA TO BECOME AVAILABLE")
-        time.sleep(0.5)
-
-    # GET ALL DATA FROM POSTGRES CACHE
-    df_file = cf.retrieve_from_cache(
-        tablename=rfq.__name__,
-        repolist=repo,
-    )
-    df_actions = cf.retrieve_from_cache(
-        tablename=cnq.__name__,
-        repolist=searchbar_repos,
-    )
-    df_file_cntrbs = cf.retrieve_from_cache(
-        tablename=cpfq.__name__,
-        repolist=repo,
-    )
-
-    # necessary preprocessing steps that were lifted out of the querying step
-    df_actions = preproc_u.contributors_df_action_naming(df_actions)
-    df_file_cntrbs = preproc_u.cntrb_per_file(df_file_cntrbs)
-
-    return df_file, df_actions, df_file_cntrbs
-
-
-def process_data(
-    df_file: pd.DataFrame,
-    df_actions: pd.DataFrame,
-    df_file_cntbs: pd.DataFrame,
-    directory,
-    bot_switch,
-):
-    """
-    Processing steps
-
-        1 - Cleans up file data to only include current files and relate files in the repository to the contributors who have reviewed them in past PRs.
-        2 - For a given level in the directory tree, aggregate the list of contributors for sub-directories and for individual files at the level.
-        3 - For each contributor, identify their most recent contribution.
-        4 - Transforms dataframe where columns are months with counts of "last seen" dates in that month and the rows are the file/subdirectory
-    """
-
-    df_file = df_file_clean(df_file, df_file_cntbs, bot_switch)
-
-    df_dynamic_directory = cntrb_per_directory_value(directory, df_file)
-
-    # work around for using functions, will clean later
-    if df_dynamic_directory.empty:
-        return df_dynamic_directory
-
-    df_dynamic_directory = cntrb_to_last_activity(df_actions, df_dynamic_directory)
-
-    final = file_cntrb_activity_by_month(df_dynamic_directory, df_actions)
-
-    return final
-
-
-def create_figure(df: pd.DataFrame):
-    fig = px.imshow(
-        df,
-        labels=dict(x="Time", y="Directory Entries", color="Contributors"),
-        color_continuous_scale=px.colors.sequential.deep,
-    )
-
-    fig["layout"]["yaxis"]["tickmode"] = "linear"
-    fig["layout"]["height"] = 700
-    fig["layout"]["coloraxis_colorbar_x"] = -0.15
-    fig["layout"]["yaxis"]["side"] = "right"
-
-    # Custom hover template to replace NaN with "No data"
-    fig.for_each_trace(
-        lambda trace: trace.update(
-            customdata=df.map(lambda x: "No data" if pd.isna(x) else x),
-            hovertemplate="<b>%{y}</b><br>" + "Time: %{x}<br>" + "Contributors: %{customdata}<extra></extra>",
-        )
-    )
-
-    return fig
-
-
-def df_file_clean(df_file: pd.DataFrame, df_file_cntbs: pd.DataFrame, bot_switch):
-    """
-    This function cleans the df_file data and combines it with the related cntrb_ids
-
-    Args:
-    -----
-        df_file : Pandas Dataframe
-            Dataframe with the output of the repo_files_query
-
-        df_file_cntrbs : Pandas Dataframe
-            Dataframe with the output of the cntrb_per_file_query
-
-        bot_switch : boolan
-            T/F for the status of the bot switch
-
-    Returns:
-    --------
-        df_file: df with file and cntrb_ids of contributors that reviewed a pr with that file in it
-    """
-    # strings to hold the values for each column (always the same for every row of this query)
-    repo_name = df_file["repo_name"].iloc[0]
-    repo_path = df_file["repo_path"].iloc[0]
-    repo_id = str(df_file["repo_id"].iloc[0])
-
-    # Handle None values by using empty strings
-    repo_name = repo_name if repo_name is not None else ""
-    repo_path = repo_path if repo_path is not None else ""
-
-    # pattern found in each file path, used to slice to get only the root file path
-    path_slice = repo_id + "-" + repo_path + "/" + repo_name + "/"
-    # Save original file_path before splitting in case split fails
-    original_file_path = df_file["file_path"].copy()
-    df_file["file_path"] = df_file["file_path"].str.rsplit(path_slice, n=1).str[1]
-    # Fill NaN values (from failed splits) with original file_path and ensure all values are strings
-    df_file["file_path"] = df_file["file_path"].fillna(original_file_path).astype(str)
-
-    # drop unneccessary columns not needed after preprocessing steps
-    df_file = df_file.reset_index()
-    df_file = df_file.drop(["index", "repo_name", "repo_path", "rl_analysis_date"], axis=1)
-
-    # split file path by directory
-    df_file = df_file.join(df_file["file_path"].str.split("/", expand=True))
-
-    # drop unnecessary columns
-    df_file = df_file.drop(["repo_id"], axis=1)
-    df_file_cntbs = df_file_cntbs.drop(["repo_id", "reviewer_ids"], axis=1)
-
-    # Left join on df_files to only get the files that are currently in the repository
-    # and the contributors that have ever reviewed a pr that included edits on the file
-    df_file = pd.merge(df_file, df_file_cntbs, on="file_path", how="left")
-    # replace nan with empty string to avoid errors in list comprehension
-    df_file["cntrb_ids"] = df_file["cntrb_ids"].fillna("")
-
-    # reformat cntrb_ids to list and remove bots if filter is on
-    if bot_switch:
-        df_file["cntrb_ids"] = df_file.apply(
-            lambda row: [x for x in row.cntrb_ids if x not in app.bots_list],
-            axis=1,
-        )
-    else:
-        df_file["cntrb_ids"] = df_file.apply(
-            lambda row: [x for x in row.cntrb_ids],
-            axis=1,
-        )
-
-    return df_file
-
-
-def cntrb_per_directory_value(directory, df_file):
-    """
-    This function gets the files in the specified directory, groups together any files in
-    subdirectories, and creates a list of their contributors cntrb_ids
-
-    Args:
-    -----
-        directory : string
-            Output from the directory drop down
-
-        df_file : Pandas Dataframe
-            Dataframe with file and related cntrb_id information
-
-    Returns:
-    --------
-        df_dynamic_directory: df with the file and subdirectories and their reviewers cntrb_ids
-    """
-    # Handle None directory by defaulting to top level
-    if directory is None or directory == "Top Level Directory":
-        level = -1
-        directory = ""
-    else:
-        # determine directory level to use in later step
-        level = directory.count("/")
-
-    # get all of the files in the directory or nested in folders in the directory
-    df_dynamic_directory = df_file[df_file["file_path"].str.startswith(directory)]
-
-    # number of files in the directory or nested in folders in the directory that have no contributors
-    num_empty_cntrb = df_dynamic_directory[df_dynamic_directory["cntrb_ids"].str.len() == 0].shape[0]
-
-    # return empty df if all of the files in the directory or nested in folders in the directory have
-    # no contributors
-    if num_empty_cntrb == df_dynamic_directory.shape[0]:
-        return pd.DataFrame()
-
-    # get one level up from the directory level
-    group_column = level + 1
-
-    # Groupby the level above the selected directory for all files nested in folders are together.
-    # For each, create a list of all of the contributors who have contributed
-    df_dynamic_directory = (
-        df_dynamic_directory.groupby(group_column)["cntrb_ids"]
-        .sum()
-        .reset_index()
-        .rename(columns={group_column: "directory_value"})
-    )
-
-    # Set of cntrb_ids to confirm there are no duplicate cntrb_ids
-    df_dynamic_directory["cntrb_ids"] = df_dynamic_directory.apply(
-        lambda row: set(row.cntrb_ids),
-        axis=1,
-    )
-    return df_dynamic_directory
-
-
-def cntrb_to_last_activity(df_actions: pd.DataFrame, df_dynamic_directory: pd.DataFrame):
-    """
-    This function creates a df with the files and the the dates of the most recent activity for each cntrb_id.
-
-    Args:
-    -----
-        df_actions : Pandas Dataframe
-            Dataframe with contributor activity
-
-        df_dynamic_directory : Pandas Dataframe
-            Dataframe with file and related cntrb_id information
-
-    Returns:
-    --------
-        df_dynamic_directory: df with the file and subdirectories and the dates of the most recent activity for the reviewers.
-    """
-
-    # date reformating
-    df_actions["created_at"] = pd.to_datetime(df_actions["created_at"], utc=True)
-
-    # sort by created_at date latest to earliest and only keep a contributors most recent activity
-    df_actions = df_actions.sort_values(by="created_at", axis=0, ascending=False)
-    df_actions = df_actions.drop_duplicates(subset="cntrb_id", keep="first")
-
-    # drop unneccessary columns not needed after preprocessing steps
-    df_actions = df_actions.reset_index()
-    df_actions = df_actions.drop(
-        ["index", "repo_id", "repo_name", "login", "Action", "rank"],
-        axis=1,
-    )
-
-    # dictionary of cntrb_ids and their most recent activity on repo
-    last_contrb = df_actions.set_index("cntrb_id")["created_at"].to_dict()
-
-    # get list of dates of the most recent activity for each contributor for each file
-    df_dynamic_directory["dates"] = df_dynamic_directory.apply(
-        lambda row: [last_contrb[x] for x in row.cntrb_ids],
-        axis=1,
-    )
-
-    # reformat into each row being a directory value and a date of one of the contributors
-    # most recent activity - preprocessing step
-    df_dynamic_directory = df_dynamic_directory.explode("dates")
-
-    return df_dynamic_directory
-
-
-def file_cntrb_activity_by_month(df_dynamic_directory: pd.DataFrame, df_actions: pd.DataFrame):
-    """
-    This function transforms the df_dynamic_directory to be counts of "last seen" contributors by month.
-
-    Args:
-    -----
-        df_actions : Pandas Dataframe
-            Dataframe with contributor activity
-
-        df_dynamic_directory : Pandas Dataframe
-            Dataframe with file and related cntrb_id information
-
-    Returns:
-    --------
-        df_final: df with files and subdirectories as rows and the months as columns
-    """
-
-    # get files that have no contributors and remove from set to prevent errors in grouper function
-    no_contribs = df_dynamic_directory["directory_value"][df_dynamic_directory.dates.isnull()].tolist()
-
-    df_dynamic_directory = df_dynamic_directory[~df_dynamic_directory.dates.isnull()]
-
-    """Creates df with a column for each month between start and end date. This will be used to confirm that
-    there will be a column for every month even if there is no "last contribution" date in it. This greatly
-    improves the heatmap ploting"""
-
-    # dates based on action so it represents the length of the project, min based on PR
-    # open date to avoid committer inputted dates
-    min_date = df_actions[df_actions["Action"] == "PR Opened"].created_at.min()
-    max_date = df_actions.created_at.max()
-    dates = pd.date_range(start=min_date, end=max_date, freq="M", inclusive="both")
-    df_fill = dates.to_frame(index=False, name="dates")
-
-    # combine df with data and filler dates together
-    final = pd.concat([df_dynamic_directory, df_fill], axis=0)
-    final["directory_value"] = final["directory_value"].astype(str)
-
-    # grouping dates by every month and counting the number of contributors with the last activity at that date
-    final = final.groupby(pd.Grouper(key="dates", freq="1M"))["directory_value"].value_counts().unstack(0)
-
-    # removing the None row that was used for column formating
-    if "nan" in final.index:
-        final = final.drop("nan")
-
-    # add back the files that had no contributors
-    for files in no_contribs:
-        final.loc[files] = None
-
-    return final
--- 8Knot/pages/codebase/visualizations/heatmap_utils.py
+++ 8Knot/pages/codebase/visualizations/heatmap_utils.py
@@ -11,9 +11,15 @@ Following DRY (Don't Repeat Yourself) principle.
 import logging
 import time
 import pandas as pd
+import plotly.express as px
 from app import augur
 import cache_manager.cache_facade as cf
 from queries.repo_files_query import repo_files_query as rfq
+from queries.contributors_query import contributors_query as cnq
+from queries.cntrb_per_file_query import cntrb_per_file_query as cpfq
+from pages.utils.job_utils import nodata_graph
+import pages.utils.preprocessing_utils as preproc_u
+import app
 
 
 def create_repo_dropdown(repo_ids, viz_id):
@@ -143,3 +149,181 @@ def create_directory_dropdown(repo_id, viz_id, max_wait_time=180):
     directories.insert(0, "Top Level Directory")
 
     return directories, "Top Level Directory"
+
+
+# ===============================================================================
+# Shared functions for activity-based heatmaps (contributor/reviewer)
+# These functions eliminate duplication between cntrb_file_heatmap.py
+# and reviewer_file_heatmap.py by providing parameterized implementations
+# ===============================================================================
+
+
+def get_activity_heatmap_data(searchbar_repos, repo, viz_id):
+    """
+    Retrieve cached data for activity-based heatmaps.
+    Shared by contributor and reviewer heatmaps.
+
+    Args:
+        searchbar_repos: List of repository IDs from searchbar
+        repo: List with single repository ID
+        viz_id: Visualization ID for logging
+
+    Returns:
+        Tuple of (df_file, df_actions, df_file_cntbs)
+    """
+    while cf.get_uncached(func_name=rfq.__name__, repolist=repo):
+        logging.warning(f"{viz_id} - WAITING ON DATA")
+        time.sleep(0.5)
+
+    while cf.get_uncached(func_name=cnq.__name__, repolist=searchbar_repos):
+        logging.warning(f"{viz_id} - WAITING ON DATA")
+        time.sleep(0.5)
+
+    while cf.get_uncached(func_name=cpfq.__name__, repolist=repo):
+        logging.warning(f"{viz_id} - WAITING ON DATA")
+        time.sleep(0.5)
+
+    df_file = cf.retrieve_from_cache(tablename=rfq.__name__, repolist=repo)
+    df_actions = cf.retrieve_from_cache(tablename=cnq.__name__, repolist=searchbar_repos)
+    df_file_cntrbs = cf.retrieve_from_cache(tablename=cpfq.__name__, repolist=repo)
+
+    df_actions = preproc_u.contributors_df_action_naming(df_actions)
+    df_file_cntrbs = preproc_u.cntrb_per_file(df_file_cntrbs)
+
+    return df_file, df_actions, df_file_cntrbs
+
+
+def process_activity_heatmap(df_file, df_actions, df_file_cntbs, directory, bot_switch, id_column):
+    """
+    Process data for activity heatmaps.
+    Shared by contributor and reviewer heatmaps.
+
+    Args:
+        df_file: DataFrame with file data
+        df_actions: DataFrame with contributor activity
+        df_file_cntbs: DataFrame with IDs per file
+        directory: Selected directory
+        bot_switch: Boolean for bot filtering
+        id_column: 'cntrb_ids' or 'reviewer_ids'
+
+    Returns:
+        DataFrame with files as rows, months as columns
+    """
+    df_file = prepare_file_data(df_file, df_file_cntbs, bot_switch, id_column)
+    df_dynamic = aggregate_by_directory_level(directory, df_file, id_column)
+
+    if df_dynamic.empty:
+        return df_dynamic
+
+    df_dynamic = map_to_last_activity_date(df_actions, df_dynamic, id_column)
+    return create_monthly_activity_matrix(df_dynamic, df_actions)
+
+
+def prepare_file_data(df_file, df_file_cntbs, bot_switch, id_column):
+    """Clean and merge file data with contributor/reviewer IDs."""
+    repo_name = df_file["repo_name"].iloc[0] if df_file["repo_name"].iloc[0] else ""
+    repo_path = df_file["repo_path"].iloc[0] if df_file["repo_path"].iloc[0] else ""
+    repo_id = str(df_file["repo_id"].iloc[0])
+
+    path_slice = f"{repo_id}-{repo_path}/{repo_name}/"
+    original = df_file["file_path"].copy()
+    df_file["file_path"] = df_file["file_path"].str.rsplit(path_slice, n=1).str[1]
+    df_file["file_path"] = df_file["file_path"].fillna(original).astype(str)
+
+    df_file = df_file.reset_index(drop=True)
+    df_file = df_file.drop(["repo_name", "repo_path", "rl_analysis_date", "repo_id"], axis=1)
+    df_file = df_file.join(df_file["file_path"].str.split("/", expand=True))
+
+    drop_col = "cntrb_ids" if id_column == "reviewer_ids" else "reviewer_ids"
+    df_file_cntbs = df_file_cntbs.drop(["repo_id", drop_col], axis=1)
+
+    df_file = pd.merge(df_file, df_file_cntbs, on="file_path", how="left")
+    df_file[id_column] = df_file[id_column].fillna("")
+
+    if bot_switch:
+        df_file[id_column] = df_file.apply(lambda row: [x for x in row[id_column] if x not in app.bots_list], axis=1)
+    else:
+        df_file[id_column] = df_file.apply(lambda row: [x for x in row[id_column]], axis=1)
+
+    return df_file
+
+
+def aggregate_by_directory_level(directory, df_file, id_column):
+    """Aggregate IDs by directory level."""
+    if directory is None or directory == "Top Level Directory":
+        level, directory = -1, ""
+    else:
+        level = directory.count("/")
+
+    df_dir = df_file[df_file["file_path"].str.startswith(directory)]
+    num_empty = df_dir[df_dir[id_column].str.len() == 0].shape[0]
+
+    if num_empty == df_dir.shape[0]:
+        return pd.DataFrame()
+
+    group_col = level + 1
+    df_dir = df_dir.groupby(group_col)[id_column].sum().reset_index().rename(columns={group_col: "directory_value"})
+
+    df_dir[id_column] = df_dir.apply(lambda row: set(row[id_column]), axis=1)
+    return df_dir
+
+
+def map_to_last_activity_date(df_actions, df_dir, id_column):
+    """Map IDs to their last activity dates."""
+    df_actions["created_at"] = pd.to_datetime(df_actions["created_at"], utc=True)
+    df_actions = df_actions.sort_values(by="created_at", ascending=False)
+    df_actions = df_actions.drop_duplicates(subset="cntrb_id", keep="first")
+
+    df_actions = df_actions.reset_index(drop=True)
+    df_actions = df_actions.drop(["repo_id", "repo_name", "login", "Action", "rank"], axis=1)
+
+    last_activity = df_actions.set_index("cntrb_id")["created_at"].to_dict()
+    df_dir["dates"] = df_dir.apply(lambda row: [last_activity[x] for x in row[id_column]], axis=1)
+
+    return df_dir.explode("dates")
+
+
+def create_monthly_activity_matrix(df_dir, df_actions):
+    """Transform data to monthly activity counts."""
+    no_contribs = df_dir["directory_value"][df_dir.dates.isnull()].tolist()
+    df_dir = df_dir[~df_dir.dates.isnull()]
+
+    min_date = df_actions[df_actions["Action"] == "PR Opened"].created_at.min()
+    max_date = df_actions.created_at.max()
+    dates = pd.date_range(start=min_date, end=max_date, freq="M", inclusive="both")
+    df_fill = dates.to_frame(index=False, name="dates")
+
+    final = pd.concat([df_dir, df_fill], axis=0)
+    final["directory_value"] = final["directory_value"].astype(str)
+    final = final.groupby(pd.Grouper(key="dates", freq="1M"))["directory_value"].value_counts().unstack(0)
+
+    if "nan" in final.index:
+        final = final.drop("nan")
+
+    for f in no_contribs:
+        final.loc[f] = None
+
+    return final
+
+
+def create_activity_heatmap_figure(df):
+    """Create plotly heatmap figure for activity visualizations."""
+    fig = px.imshow(
+        df,
+        labels=dict(x="Time", y="Directory Entries", color="Contributors"),
+        color_continuous_scale=px.colors.sequential.deep,
+    )
+
+    fig["layout"]["yaxis"]["tickmode"] = "linear"
+    fig["layout"]["height"] = 700
+    fig["layout"]["coloraxis_colorbar_x"] = -0.15
+    fig["layout"]["yaxis"]["side"] = "right"
+
+    fig.for_each_trace(
+        lambda trace: trace.update(
+            customdata=df.map(lambda x: "No data" if pd.isna(x) else x),
+            hovertemplate="<b>%{y}</b><br>Time: %{x}<br>Contributors: %{customdata}<extra></extra>",
+        )
+    )
+
+    return fig
